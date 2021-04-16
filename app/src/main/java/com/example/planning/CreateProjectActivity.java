225cdcb8877f5ddf6b35d72bfa5db51e59176f94
Serhii-Voloshyn <serhii.voloshyn59@gmail.com>
2021-04-16 21:45:50 +0300

    useless diagram

--- app/src/main/java/com/example/planning/CreateProjectActivity.java
+++ app/src/main/java/com/example/planning/CreateProjectActivity.java
@@ -2,7 +2,17 @@ package com.example.planning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class CreateProjectActivity extends AppCompatActivity {
 
@@ -10,5 +20,28 @@ public class CreateProjectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_project);
+
+        String[] investors = {"Petro", "Oleg", "Nataly"};
+        int[] invest = {100, 150, 400};
+
+        PieChart chart = findViewById(R.id.pie_Chart);
+
+        ArrayList<PieEntry> cost = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++){
+            cost.add(new PieEntry(invest[i], investors[i]));
+        }
+
+        PieDataSet pieDataSet = new PieDataSet(cost, "Cost stream");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        chart.setData(pieData);
+        chart.getDescription().setEnabled(false);
+        chart.setCenterText("Cost stream");
+        chart.animate();
     }
 }
